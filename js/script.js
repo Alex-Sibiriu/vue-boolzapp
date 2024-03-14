107b0f38dad416b9b7d11b25cfbeca5d7c40afac
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-03-14 14:31:28 +0100

    [add] Milestone 3

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ createApp({
     return {
       contacts,
       activeChat: 0,
+      newText: '',
     }
   },
 
@@ -16,6 +17,28 @@ createApp({
       const {messages} = this.contacts[index];
 
       return messages[messages.length - 1].message
+    },
+
+    sendMsg(text, index) {
+      const newMsg = {
+        date: '10/01/2020 15:30:55',
+        message: this.newText,
+        status: 'sent'
+      }
+
+      const replyMsg = {
+        date: '10/01/2020 15:30:55',
+        message: 'Ok!',
+        status: 'received'
+      }
+
+      this.contacts[index].messages.push(newMsg);
+
+      setTimeout(() => {
+        this.contacts[index].messages.push(replyMsg)
+      }, 1000);
+
+      this.newText = ''
     }
   },
 
